83aa26d8287a10ea8c669da3766e1712723032ed
Emmanuel Bastien <os@ebastien.name>
2015-01-24 18:35:49 +0100

    Move public fabricators to the lib folder.

--- spec/support/fabrication.rb
+++ spec/support/fabrication.rb
@@ -2,5 +2,8 @@
 require 'fabrication'
 
 Fabrication.configure do |config|
-  config.path_prefix = Rails.root.join("../..")
+  config.path_prefix = [
+    Rails.root.join("../.."),
+    Rails.root.join("../../lib/ti_refdata")
+  ]
 end
