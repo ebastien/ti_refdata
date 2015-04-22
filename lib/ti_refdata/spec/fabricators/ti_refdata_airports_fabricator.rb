112740fb77a6cd177b29dd9c489af2d67189a3d8
JulienCojan <julien.cojan@amadeus.com>
2015-04-22 14:51:14 +0200

    created fabricator helpers for normalized_name

--- lib/ti_refdata/spec/fabricators/ti_refdata_airports_fabricator.rb
+++ lib/ti_refdata/spec/fabricators/ti_refdata_airports_fabricator.rb
@@ -1,4 +1,11 @@
 # encoding: utf-8
+require 'ti_refdata/por_manager'
+
+class TiRefdata::Airport
+  def name_normalized
+    read_attribute(:name_normalized) || TiRefdata::PorManager.normalize_name(name)
+  end
+end
 
 Fabricator(:airport, class_name: TiRefdata::Airport) do
   code 'CDG'
